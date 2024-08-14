fb547fe514b2d8d224e91f6b2951cb8b800823f0
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-14 06:02:44 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -1,8 +1,8 @@
 from os import getenv
 
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = getenv("26267563")
+API_HASH = getenv("6665b1e3a30bb824973bf1b8b1603bb9")
+BOT_TOKEN = getenv("6381141611:AAGJM15_TQAQg6YezcPswigPukDJf1Czfyg")
 BOT_COMMANDS = (
     ("start", "Initialize the bot and check its status"),
     ("settings", "Configure and manage bot settings"),
@@ -10,6 +10,6 @@ BOT_COMMANDS = (
     ("about", "Learn more about the bot and its features"),
 )
 
-DATABASE_URL = getenv("DATABASE_URL", None)
+DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://vasudurainc:vasudurainc123@cluster01.2bypjge.mongodb.net/?retryWrites=true&w=majority")
 HOST = getenv("HOST", "0.0.0.0")
 PORT = int(getenv("PORT", "8080"))
